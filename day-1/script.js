2f8e6fe49c9bb38b626b8afe7021cbd2393b0790
Gabriel Torquato <gabrielcvtorquato@gmail.com>
2022-12-15 00:46:59 -0300

    First Commit

---
+++ day-1/script.js
@@ -0,0 +1,24 @@
+let numberOne = 1;
+let stringOne = '1';
+let numberFifty = 50;
+let stringFifty = '50';
+let numberTwelve = 12;
+let stringTwelve = '12';
+
+if(numberOne == stringOne) {
+    console.log('As variáveis numberOne e stringOne tem o mesmo valor, mas tipos diferentes');
+} else {
+    console.log('As variáveis numberOne e stringOne não tem o mesmo valor');
+}
+
+if(numberFifty === stringFifty) {
+    console.log('As variáveis numberFifty e stringFifty tem o mesmo valor e mesmo tipo');
+} else {
+    console.log('As variáveis numberFifty e stringFifty não tem o mesmo tipo');
+}
+
+if(numberTwelve == stringTwelve) {
+    console.log('As variáveis numberTwelve e stringTwelve tem o mesmo valor, mas tipos diferentes');
+} else {
+    console.log('As variáveis numberTwelve e stringTwelve não tem o mesmo valor');
+}
